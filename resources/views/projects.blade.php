7b0a4c042a654427147149abd0e87a4c75603b9b
alexxdevss <alexxdevss@gmail.com>
2022-01-21 19:42:57 +0000

    add projects view and fix some errors

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.layout')
+@section('title', "Proyectos | alexdevs")
+@section('view', 'projects')
+@section('main')
+<div class="container">
+    <div class="projects">
+        <div class="white-box shadow-lg p-lg-5 py-5 project">
+            <h1 class="text-center mb-5">AUTOS ROSO</h1>
+            <div class="row m-3">
+                <div class="col-lg-6 col-12">
+                    <p class="mb-5">
+                        <span>Autos Roso</span> es una empresa de compra y venta de vehículos con sede
+                        en diferentes islas del archipiélago Canario.
+                        Este fue mi primer trabajo publicado y se dio a través de
+                        una colaboración con la agencia <span>Wirehammer</span>.
+                    </p>
+                    <ul class="project-stack p-sm-0">
+                        <li class="d-flex justify-content-start align-items-center">
+                            <img src="{{asset('img/key_icons.png')}}" class="mr-3">
+                            <p class="mb-0">Sector automovilístico</p>
+                        </li>
+                        <li class="d-flex justify-content-start align-items-center">
+                            <img src="{{asset('img/key_icons.png')}}" class="mr-3">
+                            <p class="mb-0">Solución landing page + ERP (Backpanel personalizado)</p>
+                        </li>
+                        <li class="d-flex justify-content-start align-items-center">
+                            <img src="{{asset('img/key_icons.png')}}" class="mr-3">
+                            <p class="mb-0">Java y Google App Engine</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-12 d-flex justify-content-center align-items-center mt-lg-0 mt-5">
+                    <img src="{{asset('img/ar.png')}}" class="img-fluid">
+                </div>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <a href="https://autosroso.com/" class="button rounded-pill px-5 py-2 mt-4 animation" target="_blank">
+                    Ver Autos Roso
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="d-100"></div>
+<div class="d-100"></div>
+<div class="banner mt-5  d-flex flex-column justify-content-center align-items-start">
+    <div class="filter"></div>
+    <h1 class="wow fadeInDown text-center w-100 mb-5">¿A QUÉ ESPERAS PARA SER EL SIGUIENTE?</h1>
+    <div class="d-flex justify-content-center align-items-center w-100 mt-5">
+        <a href="/contact" class="button rounded-pill px-5 py-2 mt-4 animation">
+            CONTACTA YA
+        </a>
+    </div>
+</div>
+<div class="d-100"></div>
+<div class="d-100"></div>
+<div class="d-100"></div>
+<div class="d-100"></div>
+<div class="d-50"></div>
+@endsection
