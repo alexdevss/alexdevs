8b17cba72bff4fbaba9ba5b84b6603740f5eb1fd
alexxdevss <alexxdevss@gmail.com>
2022-02-05 17:34:17 +0000

    email

--- resources/views/mail/client.blade.php
+++ resources/views/mail/client.blade.php
@@ -1,12 +1,12 @@
 <div style="margin: 0 100px">
     <div style="background-color:#ffcc66;text-align:center;padding:30px 0">
         <h1 style="color: #fff;">¡Bienvenido a alexdevs, {{$name}}!</h1>
-        <h4 style="color: #fff;">Si este es tu problema:</h4>
-        <h4 style="color: #fff;padding:20px 50px">{{$description}}</h4>
-        <h4 style="color: #fff">¡Encontraremos una solución perfecta!</h4>
-        <h4 style="color: #fff">Me pondré en contacto contigo lo antes posible a través de tu correo: {{$mail}}</h4>
-        <h3 style="color: #fff;">¡Muchas gracias!</h3>
+        <h3 style="color: #fff;">Si este es tu problema:</h3>
+        <h3 style="color: #fff;padding:20px 50px">{{$description}}</h3>
+        <h3 style="color: #fff">¡Encontraremos una solución perfecta!</h3>
+        <h3 style="color: #fff">Me pondré en contacto contigo lo antes posible a través de tu correo: {{$mail}}</h3>
+        <h2 style="color: #fff;">¡Muchas gracias!</h2>
         <img src="{{asset('img/negative_rounded_brand.png')}}" alt="alexdevs_logo"
-            style="height:50px;width:50px;margin-top:30px">
+            style="height:150px;width:150px;margin-top:20px">
     </div>
 </div>
