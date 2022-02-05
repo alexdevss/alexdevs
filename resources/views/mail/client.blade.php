19c35ff0a8d3ec6571d74db7a660ab47ec84c4cb
alexxdevss <alexxdevss@gmail.com>
2022-02-05 16:42:57 +0000

    improving email design

--- resources/views/mail/client.blade.php
+++ resources/views/mail/client.blade.php
@@ -1,6 +1,12 @@
-<div style="background-color: #ffcc66; text-align:center; padding: 30px 0">
-    <h1 style="color: #fff;">¡Bienvenido a alexdevs!</h1>
-    <h3 style="color: #fff;">Muchas gracias por contactarme</h3>
-    <p style="color: #fff;">Me pondré en contacto contigo lo antes posible</p>
-    <img src="{{asset('img/rounded_brand.png')}}" alt="alexdevs_logo" style="margin-top: 30px">
+<div style="background-color:#ffcc66;text-align:center;padding:30px 0">
+    <h1 style="color: #fff;">¡Bienvenido a alexdevs, {{$name}}!</h1>
+    <p style="color: #fff;">
+        Si este es tu problema:
+    <blockquote>{{$description}}</blockquote>
+    ¡Encontraremos una solución perfecta!
+    </p>
+    <p>Me pondré en contacto contigo lo antes posible</p>
+    <h3 style="color: #fff;">Muchas gracias</h3>
+    <img src="{{asset('img/negative_rounded_brand.png')}}" alt="alexdevs_logo"
+        style="height:50px;width:50px;margin-top:30px">
 </div>
