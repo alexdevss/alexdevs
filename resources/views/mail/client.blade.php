62cb48548e9dd29936a33a07813b191edb543e34
alexxdevss <alexxdevss@gmail.com>
2022-02-26 12:57:09 +0000

    add new layout email

--- resources/views/mail/client.blade.php
+++ resources/views/mail/client.blade.php
@@ -1,4 +1,4 @@
-<div style="margin: 0 100px">
+<div style="margin: 0 20px">
     <div style="background-color:#ffcc66;text-align:center;padding:30px 0">
         <h1 style="color: #fff;">¡Bienvenido a alexdevs, {{$name}}!</h1>
         <h3 style="color: #fff;">Si este es tu problema:</h3>
@@ -7,6 +7,6 @@
         <h3 style="color: #fff">Me pondré en contacto contigo lo antes posible a través de tu correo: {{$mail}}</h3>
         <h2 style="color: #fff;">¡Muchas gracias!</h2>
         <img src="{{asset('img/negative_rounded_brand.png')}}" alt="alexdevs_logo"
-            style="height:150px;width:150px;margin-top:20px">
+             style="height:150px;width:150px;margin-top:20px">
     </div>
 </div>
--- resources/views/mail/new_client.blade.php
+++ resources/views/mail/new_client.blade.php
@@ -1,4 +1,4 @@
-<div style="margin: 0 50px">
+<div style="margin: 0 20px">
     <div style="background-color:#ffcc66;text-align:center;padding:30px 0">
         <h1 style="color: #fff;">¡Hola titán!</h1>
         <h3 style="color: #fff;">Tienes un nuevo cliente y sus datos son:</h3>
