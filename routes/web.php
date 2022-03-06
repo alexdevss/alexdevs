cfd44a2fdb0193b599d58d6c606ba3e4737c684e
alexxdevss <alexxdevss@gmail.com>
2022-03-06 20:25:24 +0000

    fontdisplay and cache-control

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {return view('welcome');});
-Route::get('/services', function () {return view('services');});
-Route::get('/projects', function () {return view('projects');});
-Route::get('/about', function () {return view('about');});
-Route::get('/contact/{sended?}', function ($sended = false) {return view('contact')->with('sended', $sended);})->name('/contact');
-Route::get('/awfulMessage', function () {return view('awful_message');})->name('/awful_message');
-Route::get('/legality', function () {return view('legality');});
-Route::get('/privacity', function () {return view('privacity');});
-Route::get('/cookies', function () {return view('cookies')->with('cookies', Config::get('constants.cookies'));});
-
-// Mail routes
-Route::post('/sendEmail', [EmailController::class, 'sendEmail']);
-
+Route::middleware('cache.headers:public;max_age=31536000;etag')->group(function () {
+    Route::get('/', function () {return view('welcome');});
+    Route::get('/services', function () {return view('services');});
+    Route::get('/projects', function () {return view('projects');});
+    Route::get('/about', function () {return view('about');});
+    Route::get('/contact/{sended?}', function ($sended = false) {return view('contact')->with('sended', $sended);})->name('/contact');
+    Route::get('/awfulMessage', function () {return view('awful_message');})->name('/awful_message');
+    Route::get('/legality', function () {return view('legality');});
+    Route::get('/privacity', function () {return view('privacity');});
+    Route::get('/cookies', function () {return view('cookies')->with('cookies', Config::get('constants.cookies'));});
+    
+    // Mail routes
+    Route::post('/sendEmail', [EmailController::class, 'sendEmail']);
+});
