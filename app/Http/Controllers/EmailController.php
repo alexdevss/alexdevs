15f7719b2a2850c54188258705b7a4270849f074
alexxdevss <alexxdevss@gmail.com>
2022-10-19 20:32:20 +0100

    Some fixes

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,21 +5,26 @@ namespace App\Http\Controllers;
 use App\Mail\NotifyMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
     function sendEmail (Request $request) {
-        
+        Log::debug('------------ SENDING EMAIL -------------------');
+        Log::debug($request);
         $formData = request_to_array($request);
-        if(check_empty_request($formData) === false){
+        if(!empty_request($formData)){
             $ip = $request->ip();
             Config::set('constants.badIps', $ip);
             Mail::to(env('MAIL_MY_EMAIL'))->send(new NotifyMail(Config::get('constants.mail.subject.to_me_spy'), $formData, Config::get('constants.mail.view.to_me_spy'), $ip));
             return redirect('/awfulMessage');
         }
         
+        Log::debug(Config::get('constants.mail.subject.to_client'));
+        Log::debug(Config::get('constants.mail.view.to_client'));
         Mail::to($formData['email'])->send(new NotifyMail(Config::get('constants.mail.subject.to_client'), $formData, Config::get('constants.mail.view.to_client')));
+        Log::debug('first send');
         if(Mail::failures()){
             return redirect()->route('/contact', false);
         } else {
